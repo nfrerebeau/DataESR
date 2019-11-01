6bac80a168d244f436c5fd61575b0d1f65d728dd
nfrerebeau <33983845+nfrerebeau@users.noreply.github.com>
2019-11-01 17:31:41 +0100

    Save datasets as rda files

---
+++ data-raw/DATASET.R
@@ -0,0 +1,16 @@
+## code to prepare `DATASET` dataset goes here
+esr_status <- read.table(
+  "./data-raw/esr_status.csv",
+  header = TRUE, row.names = NULL,
+  sep = ';', quote = '"',
+  encoding = "utf-8", stringsAsFactors = FALSE
+)
+
+esr_level <- read.table(
+  "./data-raw/esr_level.csv",
+  header = TRUE, row.names = NULL,
+  sep = ",", quote = "\"",
+  encoding = "utf-8", stringsAsFactors = FALSE
+)
+
+usethis::use_data(esr_status, esr_level, overwrite = TRUE, internal = FALSE)
