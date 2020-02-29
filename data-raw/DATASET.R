2baa6368badccd48a136d0c1fef4fe69573a62fe
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2020-02-29 21:43:17 +0100

    Update datasets

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -6,10 +6,7 @@ esr_status <- read.table(
   fileEncoding = "UTF-8", encoding = "UTF-8",
   stringsAsFactors = FALSE
 )
-esr_status$id <- factor(esr_status$id)
-esr_status$label <- factor(esr_status$label)
 esr_status$deprecated <- as.logical(esr_status$deprecated)
-esr_level$level <- factor(esr_level$level)
 
 esr_level <- read.table(
   "./data-raw/esr_level.csv",
@@ -18,7 +15,5 @@ esr_level <- read.table(
   fileEncoding = "UTF-8", encoding = "UTF-8",
   stringsAsFactors = FALSE
 )
-esr_level$level <- factor(esr_level$level)
-esr_level$label <- factor(esr_level$label, levels = esr_level$label)
 
 usethis::use_data(esr_status, esr_level, overwrite = FALSE, internal = FALSE)
