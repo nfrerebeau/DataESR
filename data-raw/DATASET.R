c339340327b3928c1ebea91c60cd637b960eb98d
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2020-02-29 19:23:36 +0100

    Fix data encoding

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,15 +3,22 @@ esr_status <- read.table(
   "./data-raw/esr_status.csv",
   header = TRUE, row.names = NULL,
   sep = ",", quote = "\"",
-  encoding = "UTF-8", stringsAsFactors = FALSE
+  fileEncoding = "UTF-8", encoding = "UTF-8",
+  stringsAsFactors = FALSE
 )
+esr_status$id <- factor(esr_status$id)
+esr_status$label <- factor(esr_status$label)
 esr_status$deprecated <- as.logical(esr_status$deprecated)
+esr_level$level <- factor(esr_level$level)
 
 esr_level <- read.table(
   "./data-raw/esr_level.csv",
   header = TRUE, row.names = NULL,
   sep = ",", quote = "\"",
-  encoding = "UTF-8", stringsAsFactors = FALSE
+  fileEncoding = "UTF-8", encoding = "UTF-8",
+  stringsAsFactors = FALSE
 )
+esr_level$level <- factor(esr_level$level)
+esr_level$label <- factor(esr_level$label, levels = esr_level$label)
 
-usethis::use_data(esr_status, esr_level, overwrite = TRUE, internal = FALSE)
+usethis::use_data(esr_status, esr_level, overwrite = FALSE, internal = FALSE)
